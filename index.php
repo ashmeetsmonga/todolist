fd7db6f2539bd483b77151f32d7ef8de179a756a
Ashmeet Singh <ashmeetsingh@Ashmeets-MacBook-Air.local>
2019-11-21 22:46:48 +0530

    Designed basic todo layout

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 <body>
@@ -13,6 +14,10 @@
         <li>Go Study</li>
         <li>Go Play</li>
         </ul>
+        <form method="POST">
+            <input type="text" name="task" id="task" placeholder = "Enter task" required autocomplete = "off">
+            <input type="submit" value="Add" id="btn">
+        </form>
     </div>
     
 
