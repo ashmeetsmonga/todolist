d787719283a9b79596a837987ce821c56e2773c5
ashmeetsmonga <ashmeetsmonga@gmail.com>
2019-11-24 16:52:01 +0530

    Updates task list from starting

--- myscript.js
+++ myscript.js
@@ -1,4 +1,17 @@
 $(document).ready(function() {
+    $.ajax({
+        type: "POST",
+        url: "add.php",
+        data: {task: ''},
+        dataType: "JSON",
+        success: function (response) {
+            var tasks = '';
+            $.each(response, function (i, task) {
+                tasks = tasks + "<li>" + task[1] + "</li>";
+            });
+            $('#list').html(tasks);
+        }
+    });
     $("#postform").on('submit', function() {
         var data = {
             task: $('#task').val()
